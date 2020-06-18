faa96b87bc16cce8773b84ba210beb930f7059a1
Momen Fnnana <moamenfnnana1@gmail.com>
2020-06-18 17:14:43 +0300

    finishing all project

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,15 +36,15 @@ $("#toggole-shear-btn").click(function (e) {
   $(".shared-wrapper").toggleClass("active");
 });
 
-//google map
-$("#pickup_country").PlacePicker({
-  key: "AIzaSyAPceSsErOlOqRVJ7qIb2ZnbKTPnXb4uP0",
-  title: "اختيار الموقع",
-  btnClass: "btn btn-transparent btn-sm",
-  center: { lat: 24.7245049, lng: 46.6960525 },
-  zoom: 14,
-  success: function (data, address) {
-    $("#pickup_country").val(data.formatted_address);
-    console.log(address, data);
-  },
-});
+// //google map
+// $("#pickup_country").PlacePicker({
+//     key: "AIzaSyAPceSsErOlOqRVJ7qIb2ZnbKTPnXb4uP0",
+//     title: "اختيار الموقع",
+//     btnClass: "btn btn-transparent btn-sm",
+//     center: { lat: 24.7245049, lng: 46.6960525 },
+//     zoom: 14,
+//     success: function (data, address) {
+//       $("#pickup_country").val(data.formatted_address);
+//       console.log(address, data);
+//     },
+// });
